a1fa439d789cb893cebc4d5acd80ef4cf7c79152
PavaniSurisetty <pavsurisetty@gmail.com>
2024-08-02 13:28:52 +0530

    changes in terraform code

--- resource/ec2.tf
+++ resource/ec2.tf
@@ -2,40 +2,8 @@
 resource "aws_instance" "frontend" {
   ami           = "ami-041e2ea9402c46c32"
   instance_type = "t2.micro"
-  vpc_security_group_ids = ["sg-0111e8bfb955a1b59"]
 
   tags = {
-    Name = "frontend"
+    Name = "HelloWorld"
   }
-
-}
-resource "aws_instance" "mongo" {
-  ami           = "ami-041e2ea9402c46c32"
-  instance_type = "t2.micro"
-  vpc_security_group_ids = ["sg-0111e8bfb955a1b59"]
-
-  tags = {
-    Name = "mongo"
-  }
-
-}
-resource "aws_instance" "catalogue" {
-  ami           = "ami-041e2ea9402c46c32"
-  instance_type = "t2.micro"
-  vpc_security_group_ids = ["sg-0111e8bfb955a1b59"]
-
-  tags = {
-    Name = "catalogue"
-  }
-
-}
-resource "aws_instance" "user" {
-  ami           = "ami-041e2ea9402c46c32"
-  instance_type = "t2.micro"
-  vpc_security_group_ids = ["sg-0111e8bfb955a1b59"]
-
-  tags = {
-    Name = "user"
-  }
-
 }
