b2a6b1c490bf55811899fe374c0be64b38495735
PavaniSurisetty <pavsurisetty@gmail.com>
2024-08-02 14:27:47 +0530

    changes in terraform code

--- 3variable/main.tf
+++ 3variable/main.tf
@@ -1,12 +1,21 @@
+# string
 variable "name" {
   default = "SRIRAM"
 }
 output "name" {
   value = var.name
 }
+# number
 variable "a" {
   default = 15
 }
 output "a" {
   value = var.a
+}
+#  boolean
+variable "b" {
+  default = true
+}
+output "b" {
+  value =  " value of b is ${var.b}"
 }
