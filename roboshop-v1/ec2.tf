e924b754ca78114a5ab2bac79001a33a7b499678
PavaniSurisetty <pavsurisetty@gmail.com>
2024-08-02 13:35:53 +0530

    changes in terraform code

--- roboshop-v1/ec2.tf
+++ roboshop-v1/ec2.tf
@@ -9,6 +9,13 @@ resource "aws_instance" "frontend" {
   }
 
 }
+resource "aws_route53_record" "frontend" {
+  zone_id = "Z047424331HPGCCLF50H9"
+  name    = "frontend.heydevops.online"
+  type    = "A"
+  ttl     = 15
+  records = [aws_instance.frontend]
+}
 resource "aws_instance" "mongo" {
   ami           = "ami-041e2ea9402c46c32"
   instance_type = "t2.micro"
