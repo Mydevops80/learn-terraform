53df27835abab9bc8ed0b2e81c716f228211545b
PavaniSurisetty <pavsurisetty@gmail.com>
2024-08-02 12:58:15 +0530

    changes in ansible code

--- resource/ec2.tf
+++ resource/ec2.tf
@@ -2,6 +2,7 @@
 resource "aws_instance" "frontend" {
   ami           = "ami-041e2ea9402c46c32"
   instance_type = "t2.micro"
+  vpc_security_group_ids = ["sg-0111e8bfb955a1b59"]
 
   tags = {
     Name = "frontend"
