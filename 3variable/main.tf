faedc516855d2f4b71a3922cb60ac7ef7f9ef0f2
PavaniSurisetty <pavsurisetty@gmail.com>
2024-08-02 14:34:24 +0530

    changes in terraform code

--- 3variable/main.tf
+++ 3variable/main.tf
@@ -19,10 +19,20 @@ variable "b" {
 output "b" {
   value =  " value of b is ${var.b}"
 }
-
+# list
 variable "ab" {
   default = [12,35,"abc","hello",false]
 }
 output "ab" {
   value = "value of ab ${var.ab[3]}"
+}
+#  map
+variable "class" {
+  default = {
+    course = "Devops"
+    trainer = "Raghu Sir"
+  }
+}
+output "class" {
+  value = "i am learning ${var.class[course]} and the trainer is ${var.class[trainer]} "
 }
