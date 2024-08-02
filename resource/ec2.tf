517798e8fe288f65cd370d4ebdebbe09b8ccca23
PavaniSurisetty <pavsurisetty@gmail.com>
2024-08-02 13:06:03 +0530

    changes in terraform code

--- resource/ec2.tf
+++ resource/ec2.tf
@@ -28,7 +28,8 @@ resource "aws_instance" "catalogue" {
     Name = "catalogue"
   }
 
-}resource "aws_instance" "user" {
+}
+resource "aws_instance" "user" {
   ami           = "ami-041e2ea9402c46c32"
   instance_type = "t2.micro"
   vpc_security_group_ids = ["sg-0111e8bfb955a1b59"]
