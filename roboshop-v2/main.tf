a6ed5f956b75ee11e9e391ce5d1582322c45ba6c
PavaniSurisetty <pavsurisetty@gmail.com>
2024-08-03 12:09:05 +0530

    changes in terraform code

--- roboshop-v2/main.tf
+++ roboshop-v2/main.tf
@@ -1,6 +1,6 @@
 
 resource "aws_instance" "frontend" {
-  ami           = var.ami
+  ami           =  data.aws_ami.example
   instance_type = var.instance_type
   vpc_security_group_ids = var.vpc_security_group_ids
 
